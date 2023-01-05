f8f43d555a58807709379a9683574cf42e714001
Judy Zhou <dzhou224@uwo.ca>
2023-01-04 23:17:03 -0500

    created gradient STILL need fix them :(

--- personal-site/src/components/landing.jsx
+++ personal-site/src/components/landing.jsx
@@ -13,13 +13,17 @@ class Landing extends Component {
                         <img src='judy.jpeg' />
                         Judy,</p>
                 </div>
-
                 <div className='title'>
                     <p>a first year CS student @ <span className="highlight">University of Western</span> with a passion for front-end development!</p>
                 </div>
 
                 <div className='description'>
                     {/* <p>To me, coding expresses {this.state.keywords}</p> */}
+                </div>
+                <div className='circles'>
+                    <div className='circle1'></div>
+                    <div className='circle3'></div>
+                    <div className='circle2'></div>
                 </div>
             </div>
         );
