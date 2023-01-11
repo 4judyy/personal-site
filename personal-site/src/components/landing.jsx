82f78f09434a0047c81f7fe6a998c815147ad9f8
Judy Zhou <dzhou224@uwo.ca>
2023-01-11 00:10:41 -0500

    customized nav bar

--- personal-site/src/components/landing.jsx
+++ personal-site/src/components/landing.jsx
@@ -1,28 +1,31 @@
 import './styling/landing.css'
 import React, { Component } from 'react';
+import Projects from './projects';
 
 class Landing extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className='text'>
-                    <div className='heading'>
-                        <p>Hi! I'm Judy <img alt="Judy Zhou" src='judy.jpeg' width="50px" />,</p>
-                    </div>
-                    <div className='title'>
-                        <p>a first year CS student @ <span className="highlight">University of Western</span> with a passion for front-end development!</p>
-                    </div>
+            <div >
+                <div className="container">
+                    <div className='text'>
+                        <div className='heading'>
+                            <p>Hi! I'm Judy <img alt="Judy Zhou" src='judy.jpeg' width="50px" />,</p>
+                        </div>
+                        <div className='title'>
+                            <p>a first year CS student @ <span className="highlight">University of Western</span> with a passion for front-end development!</p>
+                        </div>
 
-                    <div className='description'>
-                        <p>To me, coding expresses my logic, my creativity, and myself.</p>
+                        <div className='description'>
+                            <p>To me, coding expresses my logic, my creativity, and myself.</p>
+                        </div>
+                    </div>
+                    <div className='circles'>
+                        <div className='circle1'></div>
+                        <div className='circle2'></div>
                     </div>
                 </div>
-                <div className='circles'>
-                    <div className='circle1'></div>
-                    <div className='circle2'></div>
-                </div>
-
+                <Projects />
             </div>
         );
     }
