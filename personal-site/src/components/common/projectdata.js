8a41f20f58e657b667aec4d441990bd93d1c1b6c
Judy Zhou <dzhou224@uwo.ca>
2023-01-18 20:04:05 -0500

    made changes to projects section

--- personal-site/src/components/common/projectdata.js
+++ personal-site/src/components/common/projectdata.js
@@ -1,7 +1,7 @@
 
 export const PROJECTS = [
     {
-        coverImg: "./mockups/voice_mockup.jpg",
+        coverImg: "./mockups/voices.png",
         name: 'Western Founders Network',
         description: "In a team of four, I helped in creating a mobile application that generates a song based on lyrics given and an singer chosen.",
         stack: "Figma, HTML, CSS, React Native",
@@ -9,10 +9,18 @@ export const PROJECTS = [
     },
 
     {
-        coverImg: "./mockups/voice_mockup.jpg",
+        coverImg: "./mockups/personalportfolio.png",
         name: 'Personal Website',
         description: "As a side project, I wanted to develop a website that encapsulated the essence of who I am through a personal portfolio!",
         stack: "Figma, HTML, CSS, React",
         role: "Software Engineer, UI/UX Designer",
+    },
+
+    {
+        coverImg: "./mockups/studysprint.png",
+        name: 'Study Sprint',
+        description: " Study Sprint - a revolutionary new way to study that combines the benefits of active recall with the excitement of mini-games, making studying fun, interactive, and effective.",
+        stack: "Figma, HTML, CSS, React, Firebase",
+        role: "UI/UX Designer, Front-End Developer",
     }
 ]
